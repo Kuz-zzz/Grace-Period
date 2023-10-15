06014adf3cf6527b6ae047382c3676bf7a6a8cac
Kuz_ <37791516+Kuz-zzz@users.noreply.github.com>
2023-10-15 21:27:00 +0500

    Add more configurable options
    Added announcement_text and announcement_color, changed default fields to be more like the original THC

--- GracePeriod/Config.cs
+++ GracePeriod/Config.cs
@@ -4,13 +4,17 @@
     {
         public string text_color = "";
         public string timer_color = "";
+        public string announcement_color = "_";
+        public string announcement_text = "";
 
         public static Config DefaultConfig()
         {
             Config vConf = new Config
             {
                 text_color = "FFFFFF",
-                timer_color = "98ce91",
+                timer_color = "a4c9a7",
+                announcement_color = "FF0000",
+                announcement_text = "The grace period has ended.\nYou are now at the mercy of yourself and others.\nYou are being forced into PvP.",
             };
 
             return vConf;
